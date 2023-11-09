df717ce26a5d3d7b03f51e52ade10d1f13811b65
Mike <mikeoeo@gmail.com>
2023-11-09 21:09:39 +0200

    additional_headers nullable column

--- laravel/database/migrations/2023_11_07_204239_create_data_providers_table.php
+++ laravel/database/migrations/2023_11_07_204239_create_data_providers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('url', 500);
             $table->string('lat_lon_format');
-            $table->json('additional_headers');
+            $table->json('additional_headers')->nullable();
             $table->boolean('active');
             $table->timestamps();
         });
--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             'url' => 'http://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation_probability&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum,precipitation_probability_max&timezone=Europe%2FMoscow&forecast_days=1',
             'active' => 1,
             'lat_lon_format' => 'latitude={$lat}&longitude={$lon}',
+            'additional_headers' => null
             //&latitude=40.64&longitude=22.93
         ]);
 
@@ -33,6 +34,7 @@ class DatabaseSeeder extends Seeder
             'url' => 'http://api.weatherapi.com/v1/forecast.json?key=4da782a41477413c9c7152342230711&days=2&aqi=no&alerts=no',
             'active' => 1,
             'lat_lon_format' => 'q={$lat},{$lon}',
+            'additional_headers' => null
             //&q=40.64,22.93
         ]);
     }
