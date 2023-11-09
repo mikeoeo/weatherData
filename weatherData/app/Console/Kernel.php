f2f7c33e7ef5c4954d618e909ca1617361b43eda
Mike <mikeoeo@gmail.com>
2023-11-09 19:24:25 +0200

    run the scheduled command in the background

--- weatherData/app/Console/Kernel.php
+++ weatherData/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:fetch-forecast-data')->daily();
+        $schedule->command('app:fetch-forecast-data')->daily()->runInBackground();
     }
 
     /**
